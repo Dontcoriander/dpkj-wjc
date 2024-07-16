2c9f787ae80309639d34d54e83d6663871ad3975
Dog.zhang <982267757@qq.com>
2024-07-16 21:14:49 +0800

    1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+   <div class="add-word">
+    <select style="width: 200px;margin-bottom: 20px" v-model="wordTitle">
+    <option value="title">标题</option>
+    <option value="author">品牌</option>
+    <option value="publisher">出版社</option>
+    </select>
+    <textarea  style="width: 500px; height: 400px"  v-model="input">
+    </textarea>
+    <button type="button" @click="handleAddWord()" style="margin-top: 20px;width: 100px;">确定</button>
+   </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+<script lang="ts" setup name="HomeView">
+import { ref } from 'vue'
+import axios from 'axios'
+const input = ref('')
+const wordTitle = ref('')
+const wordsProhibit = ref([])
 
-@Options({
-  components: {
-    HelloWorld
-  }
-})
-export default class HomeView extends Vue {}
+const handleAddWord = () => {
+  console.log(input.value)
+  console.log(wordTitle.value)
+ 
+}
 </script>
+
+<style scoped lang="less">
+.add-word {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  flex-direction: column;
+}
+</style>
